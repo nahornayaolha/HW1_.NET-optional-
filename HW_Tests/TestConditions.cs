55582ca7f9bff3b18b234e478b714b909509b29f
nahornayaolha <nahornayaolha@users.noreply.github.com>
2019-02-27 20:56:46 +0200

    «stage»

---
+++ HW_Tests/TestConditions.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Tests
+{
+    public class TestConditions
+    {
+        public ChromeDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver
+            {
+                Url = "http://automationpractice.com"
+            };
+            driver.FindElement(By.XPath("//a[@class='login']")).Click();
+            driver.FindElement(By.XPath("//input[@id='email']")).SendKeys("vish262@mail.ru");
+            driver.FindElement(By.XPath("//input[@id='passwd']")).SendKeys("qwerty");
+            driver.FindElement(By.XPath("//button[@id='SubmitLogin']")).Click();
+        }
+
+    }
+}
---
+++ HW_Tests/Tests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Tests
+{
+    class Tests : TestConditions
+    {
+        [Test]
+        public void AddFadedShortSleeveTshirtIntoCart()
+        {
+            driver.FindElement(By.XPath("//a[@class='sf-with-ul']")).Click();
+            driver.FindElement(By.XPath("//a/i[@class='icon-th-list']")).Click();
+            driver.FindElement(By.XPath("//li[@class='ajax_block_product first-in-line first-item-of-tablet-line first-item-of-mobile-line col-xs-12']//span[contains(text(),'Add to cart')]")).Click();
+            driver.FindElement(By.XPath("//span[@title='Close window']")).Click();
+            driver.FindElement(By.XPath("//a[@title='View my shopping cart']")).Click();
+        }
+    }
+}
